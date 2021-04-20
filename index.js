12661e4d2b93b2feb668dc64f393d89c52bd6bb5
Isaac Cohen <issc29@gmail.com>
2021-04-20 16:40:04 -0400

    commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const trialIssueProcess = require('./trial-issue-process.js');
-const runOpsProcess = require('./ops-process.js');
+const opsIssueProcess = require('./ops-process.js');
 
-const opsProcess = core.getInput('ops');
+const runOpsProcess = core.getInput('ops');
 
 run();
 
 async function run() {
-  if(opsProcess) {
-    await trialIssueProcess.runOpsProcess(github, core)
+  if(runOpsProcess) {
+    await opsIssueProcess.runOpsProcess(github, core)
   } else {
-    await runOpsProcess.runTrialIssueProcess(github, core)
+    await trialIssueProcess.runTrialIssueProcess(github, core)
   }
 }
